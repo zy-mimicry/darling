1cd831ba24ac0ceb38ee090ca6a11aa8e57aec67
rzheng <rzheng@sierrawireless.com>
2018-08-31 17:30:45 +0800

    NO

--- testcases/test_master.py
+++ testcases/test_master.py
@@ -6,38 +6,36 @@ import pytest
 from .report import report
 
 # @report.epic("xxxxxxxxxxxxxxxxxxxxxx")
-# @report.feature('购物车功能')  # feature定义功能
+# @report.feature('hello')
 # class TestShoppingTrolley(object):
-#     @report.story('miximixi')  # story定义用户场景
+#     @report.story('miximixi')
 #     class TestInClass():
 #         def test_in_test_class(self):
 #             pass
 #     @report.issue("https://issues.sierrawireless.com/browse/QTI9X28-4440","127.0.0.1:8000")
-#     @report.story('加入购物车')  # story定义用户场景
+#     @report.story('hello')
 #     def test_add_shopping_trolley(self):
-#         login('刘春明', '密码')  # 调用“步骤函数”
-#         with report.step("浏览商品"):  # 将一个测试用例分成几个步骤，将步骤打印到测试报告中，步骤2
-#             report.attach('商品1', '刘春明')  # attach可以打印一些附加信息
-#             report.attach('商品2', 'liuchunming')
-#         with report.step("点击商品"):  # 将一个测试用例分成几个步骤，将步骤打印到测试报告中，步骤3
+#         login('ss', 'mm')
+#         with report.step("mm"):
+#             report.attach('', '')
+#         with report.step(""):
 #             pass
-#         with report.step("校验结果"):
-#             report.attach('期望结果', '添加购物车成功')
-#             report.attach('实际结果', '添加购物车失败')
+#         with report.step("aa"):
+#             report.attach('', 's')
 #             assert 'success' == 'failed'
 
 #     @report.testcase("https://issues.sierrawireless.com/browse/QTI9X28-4440")
-#     @report.story('修改购物车')
+#     @report.story('mm')
 #     def test_edit_shopping_trolley(self):
 #         pass
 
-#     @report.mark.skipif(reason='本次不执行')
-#     @report.story('删除购物车')
+#     @report.mark.skipif(reason='sss')
+#     @report.story('aaa')
 #     def test_delete_shopping_trolley(self):
 #         pass
 
 
-# @report.step('用户登录')
+# @report.step('tt')
 # def login(user, pwd):
 #     print(user, pwd)
 
